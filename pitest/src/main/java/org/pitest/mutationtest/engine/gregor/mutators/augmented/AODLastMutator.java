9c843389bbf126836dde31ca4b4a09d3b57230aa
Samuel Benton <prodigyxable@gmail.com>
2017-11-12 19:38:22 -0600

    Implemented AOR_Last mutator; this mutator keeps the second operand in an arithmetic equation

---
+++ pitest/src/main/java/org/pitest/mutationtest/engine/gregor/mutators/augmented/AODLastMutator.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright 2010 Henry Coles
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+package org.pitest.mutationtest.engine.gregor.mutators.augmented;
+
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+import org.pitest.mutationtest.engine.MutationIdentifier;
+import org.pitest.mutationtest.engine.gregor.MethodInfo;
+import org.pitest.mutationtest.engine.gregor.MethodMutatorFactory;
+import org.pitest.mutationtest.engine.gregor.MutationContext;
+
+public enum AODLastMutator implements MethodMutatorFactory {
+
+  AOD_LAST;
+
+  @Override
+  public MethodVisitor create(final MutationContext context,
+      final MethodInfo methodInfo, final MethodVisitor methodVisitor) {
+    return new AODLastMethodVisitor(this, context, methodVisitor);
+   
+  }
+
+  @Override
+  public String getGloballyUniqueId() {
+    return this.getClass().getName();
+  }
+
+  @Override
+  public String getName() {
+    return name();
+  }
+}
+
+class AODLastMethodVisitor extends MethodVisitor {
+
+  private final MethodMutatorFactory factory;
+  private final MutationContext      context;
+    
+  AODLastMethodVisitor(final MethodMutatorFactory factory, final MutationContext context, final MethodVisitor methodVisitor) {
+      super(Opcodes.ASM6, methodVisitor);
+      this.factory = factory;
+      this.context = context;
+    
+  }
+  
+  @Override
+  public void visitInsn(int opcode) {
+      if ((opcode == Opcodes.IADD) || (opcode == Opcodes.DADD) || (opcode == Opcodes.FADD) ) {
+          replaceNotLongAddOperand();
+      }  else if ((opcode == Opcodes.ISUB) || (opcode == Opcodes.DSUB) || (opcode == Opcodes.FSUB)) {
+          replaceNotLongSubOperand();
+       } else if ((opcode == Opcodes.IMUL) || (opcode == Opcodes.DMUL) || (opcode == Opcodes.FMUL)) {
+          replaceNotLongMulOperand();
+      } else if ((opcode == Opcodes.IDIV) || (opcode == Opcodes.DDIV) || (opcode == Opcodes.FDIV)) {
+          replaceNotLongDivOperand();
+          
+       // TODO Determine why none of the REM/modulus opcodes properly work with this function
+       /* Detecint modulus operations is faulty; it makes all other mutants survive  
+          } else if ((opcode == Opcodes.IREM) || (opcode == Opcodes.DREM) || (opcode == Opcodes.FREM)) {
+          replaceNotLongRemOperand();
+       */ 
+      }  else if ((opcode == Opcodes.LADD) || (opcode == Opcodes.LSUB) || (opcode == Opcodes.LMUL) || (opcode == Opcodes.LDIV) /* || (opcode == Opcodes.LREM) */ ) {
+          replaceLongOperand();
+      }  else {
+          this.mv.visitInsn(opcode);
+      }
+  
+  }
+
+  private void replaceNotLongAddOperand() {
+      final MutationIdentifier muID = this.context.registerMutation(factory, "AOD_Mutator: removed the first operand of addition formula");
+      
+      if (this.context.shouldMutate(muID)) {
+          removeNotLongFirstOperand();
+      }
+  }
+  
+  private void replaceNotLongSubOperand() {
+      final MutationIdentifier muID = this.context.registerMutation(factory, "AOD_Mutator: removed the first operand of subtraction formula");
+      
+      if (this.context.shouldMutate(muID)) {
+          removeNotLongFirstOperand();
+      }
+  }
+  
+  private void replaceNotLongMulOperand() {
+      final MutationIdentifier muID = this.context.registerMutation(factory, "AOD_Mutator: removed the first operand of multiplication formula");
+      
+      if (this.context.shouldMutate(muID)) {
+          removeNotLongFirstOperand();
+      }
+  }
+  
+  private void replaceNotLongDivOperand() {
+      final MutationIdentifier muID = this.context.registerMutation(factory, "AOD_Mutator: removed the first operand of division formula");
+      
+      if (this.context.shouldMutate(muID)) {
+          removeNotLongFirstOperand();
+      }
+  }
+  
+  private void replaceNotLongRemOperand() {
+      final MutationIdentifier muID = this.context.registerMutation(factory, "AOD_Mutator: removed the first operand of modulus formula");
+      
+      if (this.context.shouldMutate(muID)) {
+          removeNotLongFirstOperand();
+      }
+  }
+  
+  private void replaceLongOperand() {
+      final MutationIdentifier muID = this.context.registerMutation(factory, "AOD_Mutator: removed the first operand of a formula involving longs");
+      
+      if (this.context.shouldMutate(muID)) {
+          removeLongFirstOperand();
+      }
+  }
+  
+  private void removeNotLongFirstOperand() {
+      this.mv.visitInsn(Opcodes.DUP2);
+      this.mv.visitInsn(Opcodes.POP);
+      this.mv.visitInsn(Opcodes.POP);
+      
+  }
+  
+  private void removeLongFirstOperand() {
+      this.mv.visitInsn(Opcodes.DUP2_X2);
+      this.mv.visitInsn(Opcodes.POP);
+      this.mv.visitInsn(Opcodes.POP);
+  }
+  
+}
