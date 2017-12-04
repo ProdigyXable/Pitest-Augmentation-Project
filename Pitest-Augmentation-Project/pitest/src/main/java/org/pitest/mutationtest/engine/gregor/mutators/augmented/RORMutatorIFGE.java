d85fe3f8d3fe2116b40c1a9a6a8d128237dcdb7a
Samuel Benton <prodigyxable@gmail.com>
2017-12-04 14:55:39 -0600

    Restored project
    This reverts commit 91a3c406ae6795765e0432bbb2c0d103a4a3a311.

---
+++ Pitest-Augmentation-Project/pitest/src/main/java/org/pitest/mutationtest/engine/gregor/mutators/augmented/RORMutatorIFGE.java
@@ -0,0 +1,79 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+package org.pitest.mutationtest.engine.gregor.mutators.augmented;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+import org.pitest.mutationtest.engine.gregor.AbstractJumpMutator;
+import org.pitest.mutationtest.engine.gregor.MethodInfo;
+import org.pitest.mutationtest.engine.gregor.MethodMutatorFactory;
+import org.pitest.mutationtest.engine.gregor.MutationContext;
+
+public enum RORMutatorIFGE implements MethodMutatorFactory {
+
+  ROR_IFGE_MUTATOR;
+
+  @Override
+  public MethodVisitor create(final MutationContext context,
+      final MethodInfo methodInfo, final MethodVisitor methodVisitor) {
+    return new RORMutatorIFGEMethodVisitor(this, context, methodVisitor);
+  }
+
+  @Override
+  public String getGloballyUniqueId() {
+    return this.getClass().getName();
+  }
+
+  @Override
+  public String getName() {
+    return name();
+  }
+
+}
+
+class RORMutatorIFGEMethodVisitor extends AbstractJumpMutator {
+
+  private static final Map<Integer, AbstractJumpMutator.Substitution> MUTATIONS   = new HashMap<Integer, AbstractJumpMutator.Substitution>();
+
+  static {
+      
+      // Due to how Java bytecode works, this operands will *appear* flipped when used in else conditions.
+      //Functionally, these operators work as intended
+        
+        MUTATIONS.put(Opcodes.IFLE, new AbstractJumpMutator.Substitution(Opcodes.IFGE, "ROR Mutator: Changed operator <= to >="));
+        MUTATIONS.put(Opcodes.IFEQ, new AbstractJumpMutator.Substitution(Opcodes.IFGE, "ROR Mutator: Changed operator == to >="));
+        MUTATIONS.put(Opcodes.IFLT, new AbstractJumpMutator.Substitution(Opcodes.IFGE, "ROR Mutator: Changed operator < to >="));
+        MUTATIONS.put(Opcodes.IFGT, new AbstractJumpMutator.Substitution(Opcodes.IFGE, "ROR Mutator: Changed operator > to >="));
+        MUTATIONS.put(Opcodes.IFNE, new AbstractJumpMutator.Substitution(Opcodes.IFGE, "ROR Mutator: Changed operator != to >="));
+        
+        MUTATIONS.put(Opcodes.IF_ICMPGT, new AbstractJumpMutator.Substitution(Opcodes.IF_ICMPGE, "ROR Mutator: Changed operator > to >="));
+        MUTATIONS.put(Opcodes.IF_ICMPLT, new AbstractJumpMutator.Substitution(Opcodes.IF_ICMPGE, "ROR Mutator: Changed operator < to >="));
+        MUTATIONS.put(Opcodes.IF_ICMPLE, new AbstractJumpMutator.Substitution(Opcodes.IF_ICMPGE, "ROR Mutator: Changed operator <= to >="));
+        MUTATIONS.put(Opcodes.IF_ICMPEQ, new AbstractJumpMutator.Substitution(Opcodes.IF_ICMPGE, "ROR Mutator: Changed operator == to >="));
+        MUTATIONS.put(Opcodes.IF_ICMPNE, new AbstractJumpMutator.Substitution(Opcodes.IF_ICMPGE, "ROR Mutator: Changed operator != to >="));
+  }
+
+  RORMutatorIFGEMethodVisitor(final MethodMutatorFactory factory,
+      final MutationContext context, final MethodVisitor delegateMethodVisitor) {
+    super(factory, context, delegateMethodVisitor);
+  }
+
+  @Override
+  protected Map<Integer, AbstractJumpMutator.Substitution> getMutations() {
+    return MUTATIONS;
+  }
+
+}
