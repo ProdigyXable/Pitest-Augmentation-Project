ce9d6222562d43f06635f22e87d7e417830d5308
Samuel Benton <prodigyxable@gmail.com>
2017-12-04 13:23:14 -0600

    Revert "Implemented CRCR Mutators"
    This reverts commit 78c60c4a1f3292578f607678b9968009fecc650e.

--- pitest/src/main/java/org/pitest/mutationtest/engine/gregor/config/Mutator.java
+++ pitest/src/main/java/org/pitest/mutationtest/engine/gregor/config/Mutator.java
@@ -70,6 +70,7 @@ import org.pitest.mutationtest.engine.gregor.mutators.RemoveConditionalMutator.C
 import org.pitest.mutationtest.engine.gregor.mutators.ReturnValsMutator;
 import org.pitest.mutationtest.engine.gregor.mutators.VoidMethodCallMutator;
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 
 import org.pitest.mutationtest.engine.gregor.mutators.augmented.CRCRMutatorAddOne;
@@ -94,6 +95,8 @@ import org.pitest.mutationtest.engine.gregor.mutators.augmented.OBBNORMutator;
 import org.pitest.mutationtest.engine.gregor.mutators.augmented.OBBNXORMutator;
 import org.pitest.mutationtest.engine.gregor.mutators.augmented.OBBNANDMutator;
 >>>>>>> 4161a1719ce8cb91c4af3e80479203a00f9c7d87
+=======
+>>>>>>> parent of 78c60c4... Implemented CRCR Mutators
 import org.pitest.mutationtest.engine.gregor.mutators.experimental.NakedReceiverMutator;
 import org.pitest.mutationtest.engine.gregor.mutators.experimental.RemoveIncrementsMutator;
 import org.pitest.mutationtest.engine.gregor.mutators.experimental.RemoveSwitchMutator;
@@ -107,10 +110,14 @@ public final class Mutator {
   private static final Map<String, Iterable<MethodMutatorFactory>> MUTATORS = new LinkedHashMap<String, Iterable<MethodMutatorFactory>>();
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of 78c60c4... Implemented CRCR Mutators
   private static final Map<String, Iterable<MethodMutatorFactory>> MUTATORS = new LinkedHashMap<String, Iterable<MethodMutatorFactory>>();
 
   static {
 
+<<<<<<< HEAD
 =======
     // TODO Add a new line for each new mutator added
   static {
@@ -175,6 +182,8 @@ public final class Mutator {
     add("OBBN_AND", OBBNANDMutator.OBBN_AND_MUTATOR);
       
 >>>>>>> 4161a1719ce8cb91c4af3e80479203a00f9c7d87
+=======
+>>>>>>> parent of 78c60c4... Implemented CRCR Mutators
     /**
      * Default mutator that inverts the negation of integer and floating point
      * numbers.
@@ -202,6 +211,7 @@ public final class Mutator {
      *
      */
     add("VOID_METHOD_CALLS", VoidMethodCallMutator.VOID_METHOD_CALL_MUTATOR);
+<<<<<<< HEAD
 
     /**
      * Default mutator that negates conditionals.
@@ -281,12 +291,90 @@ public final class Mutator {
     /**
      * Experimental mutator that replaces method call with this
      */
+=======
+
+    /**
+     * Default mutator that negates conditionals.
+     */
+    add("NEGATE_CONDITIONALS",
+        NegateConditionalsMutator.NEGATE_CONDITIONALS_MUTATOR);
+
+    /**
+     * Default mutator that replaces the relational operators with their
+     * boundary counterpart.
+     */
+    add("CONDITIONALS_BOUNDARY",
+        ConditionalsBoundaryMutator.CONDITIONALS_BOUNDARY_MUTATOR);
+
+    /**
+     * Default mutator that mutates increments, decrements and assignment
+     * increments and decrements of local variables.
+     */
+    add("INCREMENTS", IncrementsMutator.INCREMENTS_MUTATOR);
+
+    /**
+     * Optional mutator that removes local variable increments.
+     */
+
+    add("REMOVE_INCREMENTS", RemoveIncrementsMutator.REMOVE_INCREMENTS_MUTATOR);
+
+    /**
+     * Optional mutator that removes method calls to non void methods.
+     */
+    add("NON_VOID_METHOD_CALLS",
+        NonVoidMethodCallMutator.NON_VOID_METHOD_CALL_MUTATOR);
+
+    /**
+     * Optional mutator that replaces constructor calls with null values.
+     */
+    add("CONSTRUCTOR_CALLS", ConstructorCallMutator.CONSTRUCTOR_CALL_MUTATOR);
+
+    /**
+     * Removes conditional statements so that guarded statements always execute
+     * The EQUAL version ignores LT,LE,GT,GE, which is the default behavior,
+     * ORDER version mutates only those.
+     */
+
+    add("REMOVE_CONDITIONALS_EQ_IF", new RemoveConditionalMutator(Choice.EQUAL,
+        true));
+    add("REMOVE_CONDITIONALS_EQ_ELSE", new RemoveConditionalMutator(
+        Choice.EQUAL, false));
+    add("REMOVE_CONDITIONALS_ORD_IF", new RemoveConditionalMutator(
+        Choice.ORDER, true));
+    add("REMOVE_CONDITIONALS_ORD_ELSE", new RemoveConditionalMutator(
+        Choice.ORDER, false));
+    addGroup("REMOVE_CONDITIONALS", RemoveConditionalMutator.makeMutators());
+
+    /**
+     * Experimental mutator that removed assignments to member variables.
+     */
+    add("EXPERIMENTAL_MEMBER_VARIABLE",
+        new org.pitest.mutationtest.engine.gregor.mutators.experimental.MemberVariableMutator());
+
+    /**
+     * Experimental mutator that swaps labels in switch statements
+     */
+    add("EXPERIMENTAL_SWITCH",
+        new org.pitest.mutationtest.engine.gregor.mutators.experimental.SwitchMutator());
+
+    /**
+     * Experimental mutator that replaces method call with one of its parameters
+     * of matching type
+     */
+    add("EXPERIMENTAL_ARGUMENT_PROPAGATION",
+        ArgumentPropagationMutator.ARGUMENT_PROPAGATION_MUTATOR);
+
+    /**
+     * Experimental mutator that replaces method call with this
+     */
+>>>>>>> parent of 78c60c4... Implemented CRCR Mutators
     add("EXPERIMENTAL_NAKED_RECEIVER", NakedReceiverMutator.NAKED_RECEIVER);
 
     addGroup("REMOVE_SWITCH", RemoveSwitchMutator.makeMutators());
     addGroup("DEFAULTS", defaults());
     addGroup("STRONGER", stronger());
     addGroup("ALL", all());
+<<<<<<< HEAD
 <<<<<<< HEAD
 =======
     
@@ -303,6 +391,8 @@ public final class Mutator {
     addGroup("CRCR", crcr());
 
 >>>>>>> 4161a1719ce8cb91c4af3e80479203a00f9c7d87
+=======
+>>>>>>> parent of 78c60c4... Implemented CRCR Mutators
   }
 
   public static Collection<MethodMutatorFactory> all() {
@@ -336,6 +426,7 @@ public final class Mutator {
         IncrementsMutator.INCREMENTS_MUTATOR);
   }
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
   
 
@@ -360,6 +451,59 @@ public final class Mutator {
                 CRCRMutatorAddOne.CRCR_MUTATOR_ADD_ONE,
                 CRCRMutatorSubOne.CRCR_MUTATOR_SUB_ONE);
     }
+=======
+
+  private static Collection<MethodMutatorFactory> group(
+      final MethodMutatorFactory... ms) {
+    return Arrays.asList(ms);
+  }
+
+  public static Collection<MethodMutatorFactory> byName(final String name) {
+    return FCollection.map(MUTATORS.get(name),
+        Prelude.id(MethodMutatorFactory.class));
+  }
+
+  private static void add(final String key, final MethodMutatorFactory value) {
+    MUTATORS.put(key, Collections.singleton(value));
+  }
+
+  private static void addGroup(final String key,
+      final Iterable<MethodMutatorFactory> value) {
+    MUTATORS.put(key, value);
+  }
+
+  public static Collection<MethodMutatorFactory> fromStrings(
+      final Collection<String> names) {
+    final Set<MethodMutatorFactory> unique = new TreeSet<MethodMutatorFactory>(
+        compareId());
+
+    FCollection.flatMapTo(names, fromString(), unique);
+    return unique;
+  }
+
+  private static Comparator<? super MethodMutatorFactory> compareId() {
+    return new Comparator<MethodMutatorFactory>() {
+      @Override
+      public int compare(final MethodMutatorFactory o1,
+          final MethodMutatorFactory o2) {
+        return o1.getGloballyUniqueId().compareTo(o2.getGloballyUniqueId());
+      }
+    };
+  }
+
+  private static F<String, Iterable<MethodMutatorFactory>> fromString() {
+    return new F<String, Iterable<MethodMutatorFactory>>() {
+      @Override
+      public Iterable<MethodMutatorFactory> apply(final String a) {
+        Iterable<MethodMutatorFactory> i = MUTATORS.get(a);
+        if (i == null) {
+          throw new PitHelpError(Help.UNKNOWN_MUTATOR, a);
+        }
+        return i;
+      }
+    };
+  }
+>>>>>>> parent of 78c60c4... Implemented CRCR Mutators
 
 
   /**
