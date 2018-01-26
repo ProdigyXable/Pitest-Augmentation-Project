aa7b87f7e690482bf1261ed1603af45b30312c50
Samuel Benton <prodigyxable@gmail.com>
2018-01-26 04:27:43 -0600

    Added overridden vistMethod()

--- pitest/src/main/java/org/pitest/classinfo/ClassInfoVisitor.java
+++ pitest/src/main/java/org/pitest/classinfo/ClassInfoVisitor.java
@@ -28,41 +28,47 @@ import org.objectweb.asm.Type;
 import org.pitest.bytecode.NullVisitor;
 
 public final class ClassInfoVisitor extends MethodFilteringAdapter {
-    
+
     private final ClassInfoBuilder classInfo;
-    
+
     private ClassInfoVisitor(final ClassInfoBuilder classInfo,
             final ClassVisitor writer) {
         super(writer, BridgeMethodFilter.INSTANCE);
         this.classInfo = classInfo;
     }
-    
+
     public static ClassInfoBuilder getClassInfo(final ClassName name,
             final byte[] bytes, final long hash) {
         final ClassReader reader = new ClassReader(bytes);
         final ClassVisitor writer = new NullVisitor();
-        
+
         final ClassInfoBuilder info = new ClassInfoBuilder();
         info.id = new ClassIdentifier(hash, name);
         reader.accept(new ClassInfoVisitor(info, writer), 0);
         return info;
     }
-    
+
+    @Override
+    public MethodVisitor visitMethod(final int access, final String name,
+            final String desc, final String signature, final String[] exceptions) {
+        
+    }
+
     @Override
     public void visitSource(final String source, final String debug) {
         super.visitSource(source, debug);
-        
+
         System.out.println("New source file found!\t: " + source);
         System.out.println("");
-        
+
         this.classInfo.sourceFile = source;
     }
-    
+
     @Override
     public void visit(final int version, final int access, final String name,
             final String signature, final String superName, final String[] interfaces) {
         super.visit(version, access, name, signature, superName, interfaces);
-        
+
         System.out.println("");
         System.out.println("----- New class found -----");
         System.out.println("Class name:\t" + name);
@@ -70,18 +76,18 @@ public final class ClassInfoVisitor extends MethodFilteringAdapter {
         System.out.println("Class super name:\t" + (superName == null ? "No super class" : superName));
         System.out.println("Clas interfaces:\t" + (interfaces.length == 0 ? "No class interfaces" : Arrays.toString(interfaces)));
         System.out.println("");
-        
+
         this.classInfo.access = access;
         this.classInfo.superClass = superName;
     }
-    
+
     @Override
     public void visitOuterClass(final String owner, final String name,
             final String desc) {
         super.visitOuterClass(owner, name, desc);
         this.classInfo.outerClass = owner;
     }
-    
+
     @Override
     public void visitInnerClass(final String name, final String outerName,
             final String innerName, final int access) {
@@ -91,7 +97,7 @@ public final class ClassInfoVisitor extends MethodFilteringAdapter {
             this.classInfo.outerClass = outerName;
         }
     }
-    
+
     @Override
     public AnnotationVisitor visitAnnotation(final String desc,
             final boolean visible) {
@@ -99,28 +105,28 @@ public final class ClassInfoVisitor extends MethodFilteringAdapter {
         this.classInfo.registerAnnotation(type);
         return new ClassAnnotationValueVisitor(this.classInfo, ClassName.fromString(type));
     }
-    
+
     @Override
     public MethodVisitor visitMethodIfRequired(final int access,
             final String name, final String desc, final String signature,
             final String[] exceptions, final MethodVisitor methodVisitor) {
-        
+
         return new InfoMethodVisitor(this.classInfo, methodVisitor);
-        
+
     }
-    
+
     private static class ClassAnnotationValueVisitor extends AnnotationVisitor {
-        
+
         private final ClassInfoBuilder classInfo;
         private final ClassName annotation;
-        
+
         ClassAnnotationValueVisitor(ClassInfoBuilder classInfo,
                 ClassName annotation) {
             super(Opcodes.ASM6, null);
             this.classInfo = classInfo;
             this.annotation = annotation;
         }
-        
+
         @Override
         public void visit(String name, Object value) {
             if (name.equals("value")) {
@@ -129,19 +135,19 @@ public final class ClassInfoVisitor extends MethodFilteringAdapter {
             }
             super.visit(name, value);
         }
-        
+
         @Override
         public AnnotationVisitor visitArray(String name) {
             if (name.equals("value")) {
                 final List<Object> arrayValue = new ArrayList<Object>();
-                
+
                 return new AnnotationVisitor(Opcodes.ASM6, null) {
                     @Override
                     public void visit(String name, Object value) {
                         arrayValue.add(simplify(value));
                         super.visit(name, value);
                     }
-                    
+
                     @Override
                     public void visitEnd() {
                         ClassAnnotationValueVisitor.this.classInfo
@@ -153,7 +159,7 @@ public final class ClassInfoVisitor extends MethodFilteringAdapter {
             }
             return super.visitArray(name);
         }
-        
+
         private static Object simplify(Object value) {
             Object newValue = value;
             if (value instanceof Type) {
@@ -165,22 +171,22 @@ public final class ClassInfoVisitor extends MethodFilteringAdapter {
 }
 
 class InfoMethodVisitor extends MethodVisitor {
-    
+
     private final ClassInfoBuilder classInfo;
-    
+
     InfoMethodVisitor(final ClassInfoBuilder classInfo,
             final MethodVisitor writer) {
         super(Opcodes.ASM6, writer);
         this.classInfo = classInfo;
     }
-    
+
     @Override
     public void visitLineNumber(final int line, final Label start) {
-        
+
         this.classInfo.registerCodeLine(line);
-        
+
     }
-    
+
     @Override
     public AnnotationVisitor visitAnnotation(final String desc,
             final boolean visible) {
@@ -188,5 +194,5 @@ class InfoMethodVisitor extends MethodVisitor {
         this.classInfo.registerAnnotation(type);
         return super.visitAnnotation(desc, visible);
     }
-    
+
 }
