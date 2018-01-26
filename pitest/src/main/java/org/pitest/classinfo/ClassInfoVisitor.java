8d43879c3cfe2513dfb716031f49aefb914b7ec6
Samuel Benton <prodigyxable@gmail.com>
2018-01-26 04:06:23 -0600

    Added debug output to visitSource

--- pitest/src/main/java/org/pitest/classinfo/ClassInfoVisitor.java
+++ pitest/src/main/java/org/pitest/classinfo/ClassInfoVisitor.java
@@ -28,149 +28,154 @@ import org.pitest.bytecode.NullVisitor;
 
 public final class ClassInfoVisitor extends MethodFilteringAdapter {
 
-  private final ClassInfoBuilder classInfo;
-
-  private ClassInfoVisitor(final ClassInfoBuilder classInfo,
-      final ClassVisitor writer) {
-    super(writer, BridgeMethodFilter.INSTANCE);
-    this.classInfo = classInfo;
-  }
-
-  public static ClassInfoBuilder getClassInfo(final ClassName name,
-      final byte[] bytes, final long hash) {
-    final ClassReader reader = new ClassReader(bytes);
-    final ClassVisitor writer = new NullVisitor();
-
-    final ClassInfoBuilder info = new ClassInfoBuilder();
-    info.id = new ClassIdentifier(hash, name);
-    reader.accept(new ClassInfoVisitor(info, writer), 0);
-    return info;
-  }
-
-  @Override
-  public void visitSource(final String source, final String debug) {
-    super.visitSource(source, debug);
-    this.classInfo.sourceFile = source;
-  }
-
-  @Override
-  public void visit(final int version, final int access, final String name,
-      final String signature, final String superName, final String[] interfaces) {
-    super.visit(version, access, name, signature, superName, interfaces);
-    this.classInfo.access = access;
-    this.classInfo.superClass = superName;
-  }
-
-  @Override
-  public void visitOuterClass(final String owner, final String name,
-      final String desc) {
-    super.visitOuterClass(owner, name, desc);
-    this.classInfo.outerClass = owner;
-  }
-
-  @Override
-  public void visitInnerClass(final String name, final String outerName,
-      final String innerName, final int access) {
-    super.visitInnerClass(name, outerName, innerName, access);
-    if ((outerName != null)
-        && this.classInfo.id.getName().equals(ClassName.fromString(name))) {
-      this.classInfo.outerClass = outerName;
-    }
-  }
-
-  @Override
-  public AnnotationVisitor visitAnnotation(final String desc,
-      final boolean visible) {
-    final String type = desc.substring(1, desc.length() - 1);
-    this.classInfo.registerAnnotation(type);
-    return new ClassAnnotationValueVisitor(this.classInfo, ClassName.fromString(type));
-  }
-
-  @Override
-  public MethodVisitor visitMethodIfRequired(final int access,
-      final String name, final String desc, final String signature,
-      final String[] exceptions, final MethodVisitor methodVisitor) {
-
-    return new InfoMethodVisitor(this.classInfo, methodVisitor);
-
-  }
-
-  private static class ClassAnnotationValueVisitor extends AnnotationVisitor {
     private final ClassInfoBuilder classInfo;
-    private final ClassName        annotation;
 
-    ClassAnnotationValueVisitor(ClassInfoBuilder classInfo,
-        ClassName annotation) {
-      super(Opcodes.ASM6, null);
-      this.classInfo = classInfo;
-      this.annotation = annotation;
+    private ClassInfoVisitor(final ClassInfoBuilder classInfo,
+            final ClassVisitor writer) {
+        super(writer, BridgeMethodFilter.INSTANCE);
+        this.classInfo = classInfo;
+    }
+
+    public static ClassInfoBuilder getClassInfo(final ClassName name,
+            final byte[] bytes, final long hash) {
+        final ClassReader reader = new ClassReader(bytes);
+        final ClassVisitor writer = new NullVisitor();
+
+        final ClassInfoBuilder info = new ClassInfoBuilder();
+        info.id = new ClassIdentifier(hash, name);
+        reader.accept(new ClassInfoVisitor(info, writer), 0);
+        return info;
     }
 
     @Override
-    public void visit(String name, Object value) {
-      if (name.equals("value")) {
-        this.classInfo.registerClassAnnotationValue(this.annotation,
-            simplify(value));
-      }
-      super.visit(name, value);
+    public void visitSource(final String source, final String debug) {
+        super.visitSource(source, debug);
+
+        System.out.println("New source file found!\t: " + source);
+
+        this.classInfo.sourceFile = source;
     }
 
     @Override
-    public AnnotationVisitor visitArray(String name) {
-      if (name.equals("value")) {
-        final List<Object> arrayValue = new ArrayList<Object>();
+    public void visit(final int version, final int access, final String name,
+            final String signature, final String superName, final String[] interfaces) {
+        super.visit(version, access, name, signature, superName, interfaces);
+        this.classInfo.access = access;
+        this.classInfo.superClass = superName;
+    }
 
-        return new AnnotationVisitor(Opcodes.ASM6, null) {
-          @Override
-          public void visit(String name, Object value) {
-            arrayValue.add(simplify(value));
+    @Override
+    public void visitOuterClass(final String owner, final String name,
+            final String desc) {
+        super.visitOuterClass(owner, name, desc);
+        this.classInfo.outerClass = owner;
+    }
+
+    @Override
+    public void visitInnerClass(final String name, final String outerName,
+            final String innerName, final int access) {
+        super.visitInnerClass(name, outerName, innerName, access);
+        if ((outerName != null)
+                && this.classInfo.id.getName().equals(ClassName.fromString(name))) {
+            this.classInfo.outerClass = outerName;
+        }
+    }
+
+    @Override
+    public AnnotationVisitor visitAnnotation(final String desc,
+            final boolean visible) {
+        final String type = desc.substring(1, desc.length() - 1);
+        this.classInfo.registerAnnotation(type);
+        return new ClassAnnotationValueVisitor(this.classInfo, ClassName.fromString(type));
+    }
+
+    @Override
+    public MethodVisitor visitMethodIfRequired(final int access,
+            final String name, final String desc, final String signature,
+            final String[] exceptions, final MethodVisitor methodVisitor) {
+
+        return new InfoMethodVisitor(this.classInfo, methodVisitor);
+
+    }
+
+    private static class ClassAnnotationValueVisitor extends AnnotationVisitor {
+
+        private final ClassInfoBuilder classInfo;
+        private final ClassName annotation;
+
+        ClassAnnotationValueVisitor(ClassInfoBuilder classInfo,
+                ClassName annotation) {
+            super(Opcodes.ASM6, null);
+            this.classInfo = classInfo;
+            this.annotation = annotation;
+        }
+
+        @Override
+        public void visit(String name, Object value) {
+            if (name.equals("value")) {
+                this.classInfo.registerClassAnnotationValue(this.annotation,
+                        simplify(value));
+            }
             super.visit(name, value);
-          }
+        }
 
-          @Override
-          public void visitEnd() {
-            ClassAnnotationValueVisitor.this.classInfo
-                .registerClassAnnotationValue(
-                    ClassAnnotationValueVisitor.this.annotation,
-                    arrayValue.toArray());
-          }
-        };
-      }
-      return super.visitArray(name);
-    }
+        @Override
+        public AnnotationVisitor visitArray(String name) {
+            if (name.equals("value")) {
+                final List<Object> arrayValue = new ArrayList<Object>();
 
-    private static Object simplify(Object value) {
-      Object newValue = value;
-      if (value instanceof Type) {
-        newValue = ((Type) value).getClassName();
-      }
-      return newValue;
+                return new AnnotationVisitor(Opcodes.ASM6, null) {
+                    @Override
+                    public void visit(String name, Object value) {
+                        arrayValue.add(simplify(value));
+                        super.visit(name, value);
+                    }
+
+                    @Override
+                    public void visitEnd() {
+                        ClassAnnotationValueVisitor.this.classInfo
+                                .registerClassAnnotationValue(
+                                        ClassAnnotationValueVisitor.this.annotation,
+                                        arrayValue.toArray());
+                    }
+                };
+            }
+            return super.visitArray(name);
+        }
+
+        private static Object simplify(Object value) {
+            Object newValue = value;
+            if (value instanceof Type) {
+                newValue = ((Type) value).getClassName();
+            }
+            return newValue;
+        }
     }
-  }
 }
 
 class InfoMethodVisitor extends MethodVisitor {
-  private final ClassInfoBuilder classInfo;
 
-  InfoMethodVisitor(final ClassInfoBuilder classInfo,
-      final MethodVisitor writer) {
-    super(Opcodes.ASM6, writer);
-    this.classInfo = classInfo;
-  }
+    private final ClassInfoBuilder classInfo;
 
-  @Override
-  public void visitLineNumber(final int line, final Label start) {
+    InfoMethodVisitor(final ClassInfoBuilder classInfo,
+            final MethodVisitor writer) {
+        super(Opcodes.ASM6, writer);
+        this.classInfo = classInfo;
+    }
 
-    this.classInfo.registerCodeLine(line);
+    @Override
+    public void visitLineNumber(final int line, final Label start) {
 
-  }
+        this.classInfo.registerCodeLine(line);
 
-  @Override
-  public AnnotationVisitor visitAnnotation(final String desc,
-      final boolean visible) {
-    final String type = desc.substring(1, desc.length() - 1);
-    this.classInfo.registerAnnotation(type);
-    return super.visitAnnotation(desc, visible);
-  }
+    }
+
+    @Override
+    public AnnotationVisitor visitAnnotation(final String desc,
+            final boolean visible) {
+        final String type = desc.substring(1, desc.length() - 1);
+        this.classInfo.registerAnnotation(type);
+        return super.visitAnnotation(desc, visible);
+    }
 
 }
