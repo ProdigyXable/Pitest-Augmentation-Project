e0c6c1d323ffc03e52d5319123cb079f0436868b
Samuel Benton <prodigyxable@gmail.com>
2018-01-26 05:16:03 -0600

    Added debug output for test(), outputting which files Pitest finds (seems to output unique files)

--- pitest/src/main/java/org/pitest/classpath/ProjectClassPaths.java
+++ pitest/src/main/java/org/pitest/classpath/ProjectClassPaths.java
@@ -19,37 +19,47 @@ import org.pitest.functional.FCollection;
 
 public class ProjectClassPaths {
 
-  private final ClassPath   classPath;
-  private final ClassFilter classFilter;
-  private final PathFilter  pathFilter;
+    private final ClassPath classPath;
+    private final ClassFilter classFilter;
+    private final PathFilter pathFilter;
 
-  public ProjectClassPaths(final ClassPath classPath,
-      final ClassFilter classFilter, final PathFilter pathFilter) {
-    this.classPath = classPath;
-    this.classFilter = classFilter;
-    this.pathFilter = pathFilter;
-  }
+    public ProjectClassPaths(final ClassPath classPath,
+            final ClassFilter classFilter, final PathFilter pathFilter) {
+        this.classPath = classPath;
+        this.classFilter = classFilter;
+        this.pathFilter = pathFilter;
+    }
 
-  public Iterable<ClassName> code() {
-    return FCollection.filter(
-        this.classPath.getComponent(this.pathFilter.getCodeFilter())
-        .findClasses(this.classFilter.getCode()),
-        this.classFilter.getCode()).map(ClassName.stringToClassName());
-  }
+    public Iterable<ClassName> code() {
+        return FCollection.filter(
+                this.classPath.getComponent(this.pathFilter.getCodeFilter())
+                .findClasses(this.classFilter.getCode()),
+                this.classFilter.getCode()).map(ClassName.stringToClassName());
+    }
 
-  public Iterable<ClassName> test() {
-    return FCollection.filter(
-        this.classPath.getComponent(this.pathFilter.getTestFilter())
-        .findClasses(this.classFilter.getTest()),
-        this.classFilter.getTest()).map(ClassName.stringToClassName());
-  }
+    public Iterable<ClassName> test() {
 
-  public ClassPath getClassPath() {
-    return this.classPath;
-  }
+        Iterable<ClassName> buffer = FCollection.filter(
+                this.classPath.getComponent(this.pathFilter.getTestFilter())
+                .findClasses(this.classFilter.getTest()),
+                this.classFilter.getTest()).map(ClassName.stringToClassName());
 
-  public ClassFilter getFilter() {
-    return this.classFilter;
-  }
+        System.out.println("******************************");
+
+        for (ClassName cn : buffer) {
+            System.out.println("(Unique) Class file found:\t" + cn.asJavaName());
+        }
+
+        System.out.println("******************************");
+        return buffer;
+    }
+
+    public ClassPath getClassPath() {
+        return this.classPath;
+    }
+
+    public ClassFilter getFilter() {
+        return this.classFilter;
+    }
 
 }
